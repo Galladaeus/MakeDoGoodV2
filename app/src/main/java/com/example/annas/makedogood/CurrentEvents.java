e8a51e2f1655a4dac68bb84df7c1aaf3974815f9
annas <ashche1@students.towson.edu>
2017-10-08 01:01:40 -0400

    adding events is now "live", you can also click events one by one

--- app/src/main/java/com/example/annas/makedogood/CurrentEvents.java
+++ app/src/main/java/com/example/annas/makedogood/CurrentEvents.java
@@ -1,7 +1,10 @@
 package com.example.annas.makedogood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -31,6 +34,18 @@ public class CurrentEvents extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.events);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                                    long arg3) {
+                Intent intent = new Intent(CurrentEvents.this, SpecificEvent.class);
+
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+
+        });
     }
 
     public static void add(Event tempo) {
--- app/src/main/java/com/example/annas/makedogood/SpecificEvent.java
+++ app/src/main/java/com/example/annas/makedogood/SpecificEvent.java
@@ -2,12 +2,33 @@ package com.example.annas.makedogood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SpecificEvent extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specific_event);
+        Bundle bundle = getIntent().getExtras();
+        int i = bundle.getInt("position");
+
+        TextView one = (TextView) findViewById(R.id.textView);
+        TextView two = (TextView) findViewById(R.id.textView2);
+        TextView three = (TextView) findViewById(R.id.textView3);
+        TextView four = (TextView) findViewById(R.id.textView4);
+        TextView five = (TextView) findViewById(R.id.textView5);
+
+        one.setText(CurrentEvents.arr.get(i).name);
+        two.setText(CurrentEvents.arr.get(i).location);
+        three.setText(CurrentEvents.arr.get(i).date);
+        four.setText(CurrentEvents.arr.get(i).type);
+        five.setText(CurrentEvents.arr.get(i).description);
+
+
+
     }
+
 }
