d803bde66e83cc68f1a84b6f9b8f958e79a1e9d2
Hyun Min Cho <32492041+mylilchony@users.noreply.github.com>
2017-10-08 04:06:53 -0400

    added coordinate feature
    added/tested coordinate feature that will be used to find nearby events

--- app/src/main/java/com/example/annas/makedogood/MainActivity.java
+++ app/src/main/java/com/example/annas/makedogood/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.Button;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private FusedLocationProviderClient mFusedLocationClient;
+    private double longitude;
     // Reference to firebase database, uninitialized
     private DatabaseReference mDatabase;
 
@@ -35,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
         final Button createEventButton = (Button) findViewById(R.id.button2);
         final Button viewEventsButton = (Button) findViewById(R.id.button4);
         final Button emergencyButton = (Button) findViewById(R.id.button5);
+        mFusedLocationClient.getLastLocation()
+        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    // Logic to handle location object
+                    longitude = location.getLongitude();
+                }
+            }
+        });
 
         viewEventsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,12 +78,10 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                mFusedLocationClient.getLastLocation();
-
                 NotificationCompat.Builder mBuilder =
                         new NotificationCompat.Builder(getBaseContext())
                                 .setSmallIcon(R.drawable.cast_ic_notification_small_icon)
-                                .setContentTitle("Your location is now shared")
+                                .setContentTitle("Your location is now shared " + longitude)
                                 .setContentText("Emergency!");
 
                 NotificationManager mNotificationManager =
